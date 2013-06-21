cc401634cd5659bba93d2f7ae6adf7e6a761a92d
Fong Chin <moon1991@gmail.com>
2013-06-21 13:57:03 -0700

    init

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+
+var app = express();
+
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+var OTKEY = process.env.OTKEY;
+var OTSECRET = process.env.OTSECRET;
+
+var OpenTok = require('opentok');
+var opentok = new OpenTok.OpenTokSDK( OTKEY , OTSECRET );
+
+var urlSessions = {};
+var screenShareData;
+
+app.get('/', function(req, res){
+  res.render("index", {});
+});
+
+app.get('/room/:id', function(req, res){
+  if ( urlSessions[ req.params.id ] == undefined ){
+    opentok.createSession(function(result){
+      sessionId = result;
+      urlSessions[ req.params.id ] = sessionId;
+      sendResponse( sessionId, res, req.params.id );
+    });
+  }else{
+    sessionId = urlSessions[ req.params.id ];
+    sendResponse( sessionId, res, req.params.id );
+  }
+});
+
+app.get('/screenleap', function(req, res){
+  var post_options = {
+    host: 'api.screenleap.com',
+    path: '/v2/screen-shares',
+    method: 'post',
+    headers: {
+      'accountid' : process.env.SL_ACCOUNT_ID,
+      'authtoken' : process.env.SL_AUTH_TOKEN 
+    }
+  };
+  var post_req = http.request(post_options, function(response){
+    response.on('data', function(chunk){
+      screenShareData = JSON.parse( chunk );
+      res.contentType( 'json' );
+      res.send( chunk );
+    });
+  });
+  post_req.write("");
+  post_req.end();
+});
+
+function sendResponse( sessionId, response, id ){
+  var token = opentok.generateToken({sessionId: sessionId});
+  data = {
+    OTkey: OTKEY,
+    token: token,
+    sessionId: sessionId,
+    roomId: id
+  }
+  response.render( "room", data );
+}
+
+app.listen( 3000 );
---
+++ public/javascripts/room.js
@@ -0,0 +1,111 @@
+// Enable console logs for debugging
+TB.setLogLevel(TB.DEBUG);
+
+session = TB.initSession( sessionId );
+session.addEventListener("sessionConnected", sessionConnectedHandler);
+session.addEventListener("streamCreated", streamCreatedHandler);
+
+session.connect( apiKey, token );
+
+function sessionConnectedHandler (event) {
+  subscribeToStreams(event.streams);
+  // Create publisher and start streaming into the session
+  var publisher = TB.initPublisher(apiKey, 'myPublisherDiv');
+  session.publish(publisher);
+}
+
+function subscribeToStreams(streams) {
+    for (var i = 0; i < streams.length; i++) {
+        var stream = streams[i];
+        if (stream.connection.connectionId != session.connection.connectionId) {
+          // create the div to put the subscriber element into  
+          var div = document.createElement('div');
+          div.setAttribute('id', 'stream' + stream.streamId );
+          document.getElementById("videoDiv").appendChild( div );
+          session.subscribe(stream, 'stream' + stream.streamId );
+
+        }
+    }
+}
+
+function streamCreatedHandler(event) {
+    subscribeToStreams(event.streams);
+}
+
+var roomDataRef = new Firebase( "https://openscreen.firebaseio.com/" + roomId );
+var lastViewerUrlQuery = roomDataRef.endAt().limit(1);
+var screenShareDiv = document.getElementsByClassName("right")[0];
+
+lastViewerUrlQuery.on("child_added", function( snapshot ){
+  console.log( "=======" );
+  console.log( snapshot.val().viewerUrl );
+  var screenShareIframeSource = $("#screenShareIframe").html();
+  var template2 = Handlebars.compile( screenShareIframeSource ); 
+  var iframeHtml = template2({screenLeapViewerUrl: snapshot.val().viewerUrl });
+  console.log( iframeHtml );
+
+  if ( document.getElementById("shareButtonContainer").innerHTML != "" ){
+    document.getElementById("shareButtonContainer").innerHTML = "";
+
+    // add viewerUrl link
+    screenShareLinkNode = document.createElement("a");
+    screenShareLinkNode.href = snapshot.val().viewerUrl + "&showStop=true&showResize=true";
+    screenShareLinkNode.innerText = "open the screen share window in a new window";
+    document.getElementById("stopButtonContainer").appendChild( screenShareLinkNode );
+  }
+
+  document.getElementById("iframeDiv").innerHTML = iframeHtml;
+
+});
+
+lastViewerUrlQuery.on("child_removed", function(oldChildSnapShot){
+  console.log('clearing out the divs');
+  document.getElementById("iframeDiv").innerHTML = "";
+  document.getElementById("stopButtonContainer").innerHTML = "";  
+  document.getElementById("shareButtonContainer").innerHTML = htmlForScreenShareButton;
+  alert("Screen share ended.");
+});
+
+var screenShareData;
+function shareScreenApplet(){
+
+  var http = new XMLHttpRequest();
+  var url = '/screenleap';
+  http.open("GET", url, true); 
+
+  http.onreadystatechange = function(){
+    if (http.readyState == 4 && http.status == 200 ){
+      screenShareData = JSON.parse( http.responseText );
+      console.log( screenShareData );
+
+      screenLeapExtension( screenShareData );
+      
+      roomDataRef.push().set({ viewerUrl : screenShareData.viewerUrl });
+    }
+  }
+
+  http.send(null);  
+}
+
+function screenLeapExtension( screenShareData ){
+  screenleap.checkIsExtensionInstalled(function() {
+      console.log("checking extension");
+      screenleap.startScreenShare('EXTENSION', screenShareData );
+      document.getElementById("stopButtonContainer").innerHTML = "<p>Click stop sharing button on the extension to stop the screen share</p>";
+      
+  }, function() {
+      screenleapInstallExtension(function() {
+          screenleap.startScreenShare('EXTENSION', screenShareData);
+          document.getElementById("stopButtonContainer").innerHTML = "<p>Click stop sharing button on the extension to stop the screen share</p>";
+      }, function(errorMessage) {
+          screenShareDiv.innerHTML = "<p>Unable to install the sharing extension for " + errorMessage + ". Visit <a href='http://screenleap.com'>screenleap.com</a> to find out more about screen sharing using screenleap.</p>";
+      });
+  });
+
+}
+
+screenShareEnded = function(){
+  roomDataRef.remove();
+}
+
+screenleap.screenShareEnded = screenShareEnded;
